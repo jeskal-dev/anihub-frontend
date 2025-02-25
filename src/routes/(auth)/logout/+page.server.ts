5f089064cd5f2361ef56813e2f7dea9502a80a8b
jesus <jesus@epepo.cupet.cu>
2025-02-25 13:02:59 -0500

    ADD logout page

--- src/routes/(auth)/logout/+page.server.ts
+++ src/routes/(auth)/logout/+page.server.ts
@@ -1,5 +1,4 @@
 import { Auth } from '$lib/api/services/auth.js';
-import authStore from '$lib/stores/authStore.js';
 import { tokenStore } from '$lib/stores/tokenStore.js';
 import { redirect } from '@sveltejs/kit';
 import { get } from 'svelte/store';
@@ -8,8 +7,7 @@ export const actions = {
 	default: async function () {
 		const token = get(tokenStore);
 		if (token.accessToken) {
-			Auth.logout(token.accessToken);
-			authStore.logout();
+			Auth.logout(token.accessToken); 
 			tokenStore.clearTokens();
 		}
 
---
+++ src/routes/(auth)/logout/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import authStore from '$lib/stores/authStore';
+	import { onMount } from 'svelte';
+
+	authStore.logout();
+	onMount(() => {
+		const timeout = setTimeout(() => goto('/'), 2000);
+		return () => clearTimeout(timeout);
+	});
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center text-neutral-100">
+	<div class="animate-pulse">
+		<h1 class="text-3xl font-bold text-indigo-600">Cerrando sesión...</h1>
+		<p class="mt-4 text-neutral-300">Redirigiendo en breve.</p>
+	</div>
+</div>
